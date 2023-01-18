aac75443792ffbfe25e44b5995483b236761f17c
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-18 10:13:41 -0300

    :rocket: Server - Separando conexão

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import {prisma} from './lib/prisma'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
 
