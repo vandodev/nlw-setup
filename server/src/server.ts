e8b3283a5189eadc063ee98c5fb4ceb8979b2ca8
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-18 10:21:55 -0300

    :rocket: Server - Separando rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,11 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import {prisma} from './lib/prisma'
+import { appRoutes } from "./routes"
 
 const app = Fastify()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany()
-    return habits
-})
 
 app.listen({port: 3333}).then(() => console.log("HTTP Server Runnong!"))
