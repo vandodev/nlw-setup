1152519fbfb590579568051b67462da879b21599
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-19 19:03:14 -0300

    :rocket: Server - Retornando habitos possiveis

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -29,6 +29,34 @@ export async function appRoutes(app: FastifyInstance) {
         }
       }
     })
-  })
+})
    
+app.get('/day', async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    })
+
+    const { date } = getDayParams.parse(request.query)
+
+    const parsedDate = dayjs(date).startOf('day')
+    const weekDay = parsedDate.get('day')
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          }
+        }
+      },
+    })
+
+    return {
+      possibleHabits
+    }
+  })
+
 }
