35633229d5b15d7807047a255d3671bdf70f8d71
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-20 02:25:59 -0300

    :rocket: Mobile - Criando quadradinhos para os dias

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
+import { HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
 export function Home() {
     return(
         <View className='flex-1 bg-background px-8 pt-16'>
             <Header/>
+            <HabitDay />
         </View>
     )
 }
