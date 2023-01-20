3a7e2c48c4183f823fc196b615896dae21ad5857
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-19 22:00:40 -0300

    :rocket: web - Component SummaryTable dias da semana em array

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,29 +1,24 @@
+const weekDays = [
+  'D',
+  'S',
+  'T',
+  'Q',
+  'Q',
+  'S',
+  'S',
+];
 
 export function SummaryTable() {
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-         <div className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-            D
-         </div>
-         <div className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-            S
-         </div>
-         <div className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-            T
-         </div>
-         <div className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-            Q
-         </div>
-         <div className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-            Q
-         </div>
-         <div className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-            S
-         </div>
-         <div className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-            S
-         </div>
+        {weekDays.map((weekDay, i) =>{
+            return(
+                <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
+                    {weekDay}
+                </div>
+            )
+        })}
       </div>
     </div>
   );
