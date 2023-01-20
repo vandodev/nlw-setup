56ef94f33000158a7f0f66147f174a188817de35
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-20 02:01:39 -0300

    :rocket: Mobile - Configurando SVG

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,7 +1,10 @@
 import { View } from "react-native";
+import Logo from '../assets/logo.svg';
 
 export function Header() {
   return (
-    <View className="w-full flex-row items-center justify-between"></View>
+    <View className="w-full flex-row items-center justify-between">
+      <Logo />
+    </View>
   )
 }
