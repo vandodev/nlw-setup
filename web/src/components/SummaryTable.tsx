bc2518c4cc20e7de83c7efc9296fb0886786e895
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-19 22:18:07 -0300

    :rocket: web - Dias da semana (quadradinho)

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -14,11 +14,78 @@ export function SummaryTable() {
       <div className="grid grid-rows-7 grid-flow-row gap-3">
         {weekDays.map((weekDay, i) =>{
             return(
-                <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
+                <div key={`${weekDay}-${i}`} className="text-zinc-800 text-xl h-10 w-10 font-bold flex items-center justify-center">
                     {weekDay}
                 </div>
             )
         })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+ <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg " ></div>
       </div>
     </div>
   );
