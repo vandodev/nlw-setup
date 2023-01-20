ba1c630997182319cad1d74f49f0adf13abea969
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-20 01:36:35 -0300

    :rocket: Mobile - Configurando nativewind

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { Loading } from './src/components/Loading';
 import { 
   useFonts,
@@ -7,6 +7,7 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
+import { Home } from './src/screens/Home';
 
 export default function App() {
    const [fontsLoaded] = useFonts({
@@ -22,22 +23,9 @@ export default function App() {
     );
   }
   return (
-    <View style={styles.container}>
-      <Text>Olá mundo</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-   text: {
-    color: '#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
