2d268f4e4c63099734de8a25cda692b2cb22e0b9
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-21 11:31:38 -0300

    :rocket: Server - Rota de resumo com queryRaw

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -123,4 +123,32 @@ app.get('/day', async (request) => {
       })
     }
   })
+
+  app.get('/summary', async () => {
+const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HDW
+          JOIN habits H
+            ON H.id = HDW.habit_id
+          WHERE
+            HDW.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `
+
+    return summary
+   }
+  )
 }
