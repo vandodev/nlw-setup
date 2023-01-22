24d9b5f491649031576cadb06ee8ec982a60a2d2
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-22 01:22:12 -0300

    :rocket: Mobile - Criando botão voltar

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,7 +1,15 @@
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
+import { BackButton } from "../components/BackButton";
 
 export function New() {
     return(
-        <View className="flex-1 bg-background px-8 pt-16"></View>
+        <View className="flex-1 bg-background px-8 pt-16">
+         <ScrollView 
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          >
+            <BackButton />
+         </ScrollView>
+        </View>
     )
 }
