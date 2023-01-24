04387536606c8bdbc76465d1214d0bc3511f8e30
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-24 03:39:15 -0300

    :rocket: Mobile - Estilizando dia atual

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,5 +1,6 @@
 import { Dimensions, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import clsx from "clsx";
+import dayjs from "dayjs";
 import { generateProgressPercentage } from "../utils/generate-progress-percentage";
 
 const WEEK_DAYS = 7;
@@ -15,8 +16,11 @@ interface Props extends TouchableOpacityProps {
 }
 
 export function HabitDay({ amountOfHabits = 0, amountCompleted = 0, date, ...rest }: Props) {
+
   const amountAccomplishedPercentage = amountOfHabits > 0 ? generateProgressPercentage(amountOfHabits, amountCompleted) : 0
-  
+  const today = dayjs().startOf('day').toDate();
+  const isCurrentDay = dayjs(date).isSame(today);
+
   return (
     <TouchableOpacity 
       className={clsx(
@@ -27,6 +31,7 @@ export function HabitDay({ amountOfHabits = 0, amountCompleted = 0, date, ...res
           ["bg-violet-700 border-violet-500"] : amountAccomplishedPercentage > 40 && amountAccomplishedPercentage < 60,
           ["bg-violet-600 border-violet-500"] : amountAccomplishedPercentage > 60 && amountAccomplishedPercentage < 80,
           ["bg-violet-500 border-violet-400"] : amountAccomplishedPercentage > 80,
+          ["border-white border-4"] : isCurrentDay,
         } 
       )}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
