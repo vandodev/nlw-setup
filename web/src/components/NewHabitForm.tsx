7659bf1181421b7e53b5f172aaa9c30b17a96410
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-24 02:20:02 -0300

    :rocket: Web - Cadastrando novo hábito

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from "react";
 import { Check } from "phosphor-react";
 import * as Checkbox from '@radix-ui/react-checkbox';
+import { api } from "../lib/axios";
 
 const availableWeekDays = [
   'Domingo',
@@ -18,6 +19,20 @@ export function NewHabitForm() {
 
   async function createNewHabit(event: FormEvent) {
     event.preventDefault()
+
+    if(!title || weekDays.length === 0) {
+      return
+    }
+
+    await api.post('habits', {
+      title,
+      weekDays
+    })
+
+    setTitle('')
+    setWeekDays([])
+
+    alert('Hábito criado com sucesso!')
     console.log(title,weekDays)
   }
 
@@ -41,6 +56,7 @@ export function NewHabitForm() {
       <input 
         type="text"
         id="title"
+        value={title}
         placeholder="ex.: Exercícios, dormir bem, etc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
@@ -56,6 +72,7 @@ export function NewHabitForm() {
           <Checkbox.Root
             key={weekDay}
             className='flex items-center gap-3 group'
+            checked={weekDays.includes(index)}
             onCheckedChange={() => handleToggleWeekDay(index)}
           >
             <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50'>
