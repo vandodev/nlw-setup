94ad47ac392e268054a9fc424b775e758e178669
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-26 00:51:44 -0300

    :rocket: Mobile - Buscando hábitos selecionados do dia

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,53 +1,75 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Alert } from "react-native";
+import { useEffect, useState } from "react";
 import { useRoute } from "@react-navigation/native";
 import dayjs from "dayjs";
+import { api } from "../lib/axios";
 
 import { BackButton } from "../components/BackButton";
 import { ProgressBar } from "../components/Progress.Bar";
 import { Checkbox } from "../components/Checkbox";
+import { Loading } from "../components/Loading";
 
 interface Params {
   date: string;
 }
 
 export function Habit() {
-    const route = useRoute()
-    const { date } = route.params as Params;
+  const [loading, setLoading] = useState(true);
+  const route = useRoute();
+  const { date } = route.params as Params;
 
-    const parsedDate = dayjs(date);
-    const dayOfWeek = parsedDate.format('dddd');
-    const dayAndMonth = parsedDate.format('DD/MM');
+  const parsedDate = dayjs(date);
+  const dayOfWeek = parsedDate.format("dddd");
+  const dayAndMonth = parsedDate.format("DD/MM");
 
-    return(
-        <View className="flex-1 bg-background px-8 pt-16">
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ paddingBottom: 100 }}
-            >
-             <BackButton />
+  async function fetchHabits() {
+    try {
+      setLoading(true);
+      const response = await api.get("/day", { params: { date } });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        "Ops",
+        "Não foi possível carregar as informações dos hábitos."
+      );
+    } finally {
+      setLoading(false);
+    }
+  }
 
-              <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
-                 {dayOfWeek}
-              </Text>
+  useEffect(() => {
+    fetchHabits();
+  }, []);
 
-              <Text className="text-white font-extrabold text-3xl">
-               {dayAndMonth}
-              </Text>
+  if (loading) {
+    return <Loading />;
+  }
 
-              <ProgressBar progress={30}/>
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
 
-                <View className="mt-6">
-                    <Checkbox 
-                        title="Beber 2L de água"
-                        checked={false}
-                    />
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
 
-                    <Checkbox 
-                        title="Caminhar"
-                        checked
-                    />
-                </View>
-            </ScrollView>
+        <Text className="text-white font-extrabold text-3xl">
+          {dayAndMonth}
+        </Text>
+
+        <ProgressBar progress={30} />
+
+        <View className="mt-6">
+          <Checkbox title="Beber 2L de água" checked={false} />
+
+          <Checkbox title="Caminhar" checked />
         </View>
-    )
+      </ScrollView>
+    </View>
+  );
 }
