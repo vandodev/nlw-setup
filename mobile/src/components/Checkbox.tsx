575ffdeafb1bc6b240bfacf0e286121708846aac
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-27 00:16:25 -0300

    :rocket: Mobile - Animando Checkbox com react-native-reanimated

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,6 +1,12 @@
-import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
-import { Feather } from '@expo/vector-icons'
+import {
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 
 interface Props extends TouchableOpacityProps {
   title: string;
@@ -14,22 +20,19 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
       className="flex-row mb-2 items-center"
       {...rest}
     >
-      { checked 
-        ?
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
-          <Feather 
-            name="check"
-            size={20}
-            color={colors.white}
-          />
-        </View>
-        : 
+      {checked ? (
+        <Animated.View
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
+          <Feather name="check" size={20} color={colors.white} />
+        </Animated.View>
+      ) : (
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
-      }
+      )}
 
-      <Text className="text-white text-base ml-3 font-semibold">
-        {title}
-      </Text>
+      <Text className="text-white text-base ml-3 font-semibold">{title}</Text>
     </TouchableOpacity>
-  )
+  );
 }
